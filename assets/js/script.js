412f17127983e277848a1b90c972da032ccb936e
ajmccredie <>
2023-09-27 16:30:27 +0000

    Add script for starts of button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ const API_KEY = "eGzjynIXM26Fz-vNsDcaEto2K9c";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = '${API_URL}?api_key=${API_KEY}';
+}
