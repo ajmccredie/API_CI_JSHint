e8ae56a2054fa3391e504101ab81f97cf0c90edc
ajmccredie <>
2023-09-27 16:15:53 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const API_KEY = "eGzjynIXM26Fz-vNsDcaEto2K9c";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
