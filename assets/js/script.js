67042489823fc16a13a25dbe6009d2ae7da8a09e
ajmccredie <>
2023-09-28 11:28:49 +0000

    Add promise code for API access

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,12 @@ document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
     const queryString = '${API_URL}?api_key=${API_KEY}';
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
 }
