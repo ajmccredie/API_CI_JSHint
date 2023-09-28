63201003792dcde2a6bdb64b8cd25383ff5fedf4
ajmccredie <>
2023-09-28 12:42:37 +0000

    Add code to allow buttons to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,15 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    for (let e of form.entries()) {
+        console.log(e);
+    }
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -12,6 +21,18 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
+    } else {
+        throw new Error (data.error);
     }
+}
+
+function displayStatus(data) {
+    let title = "Status of API Key";
+    let status = `<div class="key-status">${data.expiry}<\div>`;
+
+    document.getElementById("resultsModalTitle").innerText = title;
+    document.getElementById("results-content").innerHTML = status;
+
+    resultsModal.show();
 }
